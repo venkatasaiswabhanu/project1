13b08dda9720bcb743c6a23d38a30b00f60ddf72
Venkata Sai Swabhanu <49754069+venkatasaiswabhanu@users.noreply.github.com>
2024-02-28 19:53:19 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,18 +4,20 @@ import os
 
 class TestMySQLPasswordChange(unittest.TestCase):
 
-    def read_password(file_path):
+    def read_password(self, file_path):
         with open(file_path, 'r') as file:
             return file.readline().strip()
+
     def test_mysql_login(self):
         try:
             # Read the password from pwd.txt
-            old_password = read_password(old_password_file)
+            old_password_file = "pwd.txt"
+            old_password = self.read_password(old_password_file)
 
             # Attempt MySQL connection using the password from pwd.txt
             connection = mysql.connector.connect(host='localhost',
                                                  user='root',
-                                                 password=password)
+                                                 password=old_password)
             connection.close()
             # If the connection is successful, the test passes
             self.assertTrue(True)
